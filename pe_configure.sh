64c43d90a57e8d79c788d5e6057c9c8ad2989351
Michael Arnold <github@razorsedge.org>
2014-03-13 01:52:32 -0700

    Apply UTF8 locale to Debian systems.
    Otherwise PostgreSQL refuses to run.

--- pe_configure.sh
+++ pe_configure.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-/opt/puppet/bin/puppet config set modulepath /modules:/etc/puppetlabs/puppet/modules:/opt/puppet/share/puppet/modules --section main
+/opt/puppet/bin/puppet config set modulepath /etc/puppetlabs/puppet/modules:/modules:/opt/puppet/share/puppet/modules --section main
 
 cp -p /vagrant/pe_license.key /etc/puppetlabs/license.key
 
@@ -12,4 +12,13 @@ nodegroup:add[agent5_server,profile::cloudera::agent5_server,skip] \
 nodegroup:add[agent4_server,profile::cloudera::agent4_server,skip] \
 nodegroup:add[agent5_noparcels_gplextras_cdh5,profile::cloudera::agent5_noparcels_gplextras_cdh5,skip] \
 nodegroup:add[agent5_noparcels_gplextras_cdh4,profile::cloudera::agent5_noparcels_gplextras_cdh4,skip]
+
+/opt/puppet/bin/puppet module install attachmentgenie/locales
+/opt/puppet/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile RAILS_ENV=production \
+nodeclass:add[locales,skip] \
+nodegroup:add[debian,locales,skip] \
+node:add[debian6,debian,,skip] \
+node:addgroup[debian6,debian]
+node:add[debian7,debian,,skip] \
+node:addgroup[debian7,debian]
 
