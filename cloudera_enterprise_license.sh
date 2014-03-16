9590c1e244bd9d5e0edbefc8a2d7a35a7ee12fea
Michael Arnold <github@razorsedge.org>
2014-03-16 00:01:30 -0700

    Make sure curl is installed in VMs.

--- cloudera_enterprise_license.sh
+++ cloudera_enterprise_license.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-# Install the license key.
+puppet resource package curl ensure=present
+
+echo "Install the Cloudera Manager license key."
 curl -u "admin:admin" \
   -F license=@/vagrant/puppetlabs-c5_dev_cloudera_enterprise_license.txt \
   http://localhost:7180/api/v6/cm/license
 
-# Configure CM to use the local squid proxy for parcel downloads.
+echo "Configure CM to use the local squid proxy for parcel downloads."
 curl -X PUT -u "admin:admin" \
  -H "Content-Type:application/json" \
  -d '{
