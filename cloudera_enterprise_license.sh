919c35bd6a224b6dcfa4bcfa52136025ed721628
Michael Arnold <github@razorsedge.org>
2014-03-25 22:43:34 -0700

    Make README.md and cloudera_enterprise_license.sh better.

--- cloudera_enterprise_license.sh
+++ cloudera_enterprise_license.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+USE_PROXY=y
 
 puppet resource package curl ensure=present
 
@@ -7,17 +8,19 @@ curl -u "admin:admin" \
   -F license=@/vagrant/puppetlabs-c5_dev_cloudera_enterprise_license.txt \
   http://localhost:7180/api/v6/cm/license
 
-echo "Configure CM to use the local squid proxy for parcel downloads."
-curl -X PUT -u "admin:admin" \
- -H "Content-Type:application/json" \
- -d '{
-   "items" : [ {
-     "name" : "PARCEL_PROXY_PORT",
-     "value" : "3128"
-   }, {
-     "name" : "PARCEL_PROXY_SERVER",
-     "value" : "proxy"
-   } ]
- }' \
- http://localhost:7180/api/v6/cm/config
+if [ $USE_PROXY == "y" ]; then
+  echo "Configure CM to use the local squid proxy for parcel downloads."
+  curl -X PUT -u "admin:admin" \
+   -H "Content-Type:application/json" \
+   -d '{
+     "items" : [ {
+       "name" : "PARCEL_PROXY_PORT",
+       "value" : "3128"
+     }, {
+       "name" : "PARCEL_PROXY_SERVER",
+       "value" : "proxy"
+     } ]
+   }' \
+   http://localhost:7180/api/v6/cm/config
+fi
 
