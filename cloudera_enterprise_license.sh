9121d3248a8a926e75c903d3ce6135f4ad5031a6
Michael Arnold <github@razorsedge.org>
2014-04-14 00:06:55 -0700

    Use bash for cloudera_enterprise_license.sh.

--- cloudera_enterprise_license.sh
+++ cloudera_enterprise_license.sh
@@ -1,15 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 USE_PROXY=y
 
+echo "** Install curl."
 puppet resource package curl ensure=present
 
-echo "Install the Cloudera Manager license key."
+echo "** Install the Cloudera Manager license key."
 curl -u "admin:admin" \
   -F license=@/vagrant/puppetlabs-c5_dev_cloudera_enterprise_license.txt \
   http://localhost:7180/api/v6/cm/license
+echo
 
-if [ $USE_PROXY == "y" ]; then
-  echo "Configure CM to use the local squid proxy for parcel downloads."
+if [ "$USE_PROXY" == "y" ]; then
+  echo "** Configure CM to use the local squid proxy for parcel downloads."
   curl -X PUT -u "admin:admin" \
    -H "Content-Type:application/json" \
    -d '{
@@ -22,5 +24,6 @@ if [ $USE_PROXY == "y" ]; then
      } ]
    }' \
    http://localhost:7180/api/v6/cm/config
+  echo
 fi
 
