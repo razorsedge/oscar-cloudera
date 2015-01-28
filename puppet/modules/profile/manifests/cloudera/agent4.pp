b6162edb92ccb3536b220bc8a316c0549ffc9823
Michael Arnold <github@razorsedge.org>
2015-01-27 23:29:28 -0700

    Remove duplicate Class['profile::cloudera::agent?'].

--- puppet/modules/profile/manifests/cloudera/agent4.pp
+++
@@ -1,6 +0,0 @@
-class profile::cloudera::agent4 {
-  class { '::cloudera':
-    cm_server_host => $::fqdn,
-    cm_version     => '4',
-  }
-}
--- puppet/modules/profile/manifests/cloudera/agent5.pp
+++
@@ -1,5 +0,0 @@
-class profile::cloudera::agent5 {
-  class { 'cloudera':
-    cm_server_host => $::fqdn,
-  }
-}
