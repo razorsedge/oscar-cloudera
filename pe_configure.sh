7293ebd63eee16b3550379efa4fb1a5f31214bd6
Michael Arnold <github@razorsedge.org>
2015-03-17 23:27:16 -0700

    Switch from rake tasks to calling the PE 3.7 API.

--- pe_configure.sh
+++ pe_configure.sh
@@ -61,6 +61,17 @@ node:addgroup[centos6,redhat]
 else
   # PE >= 3.7.x
   /opt/puppet/bin/puppet config set basemodulepath /etc/puppetlabs/puppet/modules:/modules:/opt/puppet/share/puppet/modules --section main
+  mkdir -p /var/lib/hiera
+  echo -e "---\npuppet_enterprise::profile::master::java_args:\n  Xmx: 512m\n  Xms: 512m" >/var/lib/hiera/global.yaml
+  cat <<EOF >>/etc/puppetlabs/puppet/environments/production/manifests/site.pp
+# https://ask.puppetlabs.com/question/6640/warning-the-package-types-allow_virtual-parameter-will-be-changing-its-default-value-from-false-to-true-in-a-future-release/
+if versioncmp($::puppetversion,'3.6.1') >= 0 {
+  \$allow_virtual_packages = hiera('allow_virtual_packages',false)
+  Package {
+    allow_virtual => \$allow_virtual_packages,
+  }
+}
+EOF
 
   /opt/puppet/bin/puppet module install attachmentgenie/locales
   /opt/puppet/bin/puppet module install spiette/selinux
@@ -68,41 +79,142 @@ else
   sleep 60
 
 #  curl https://master:4433/classifier-api/v1/groups --cert /etc/puppetlabs/puppet/ssl/certs/pe-internal-dashboard.pem --key /etc/puppetlabs/puppet/ssl/private_keys/pe-internal-dashboard.pem --cacert /etc/puppetlabs/puppet/ssl/certs/ca.pem -H "Content-Type: application/json"
-  curl https://master:4433/classifier-api/v1/update-classes -d key=value --cert /etc/puppetlabs/puppet/ssl/certs/pe-internal-dashboard.pem --key /etc/puppetlabs/puppet/ssl/private_keys/pe-internal-dashboard.pem --cacert /etc/puppetlabs/puppet/ssl/certs/ca.pem -H "Content-Type: application/json"
+  CURL_OPTS='--cert /etc/puppetlabs/puppet/ssl/certs/pe-internal-dashboard.pem --key /etc/puppetlabs/puppet/ssl/private_keys/pe-internal-dashboard.pem --cacert /etc/puppetlabs/puppet/ssl/certs/ca.pem'
+  CURL_OPTS_CONTENT='-H "Content-Type:application/json"'
+  echo "** Updating Node Classifier Classes"
+  curl $CURL_OPTS -X POST https://master:4433/classifier-api/v1/update-classes
 
-  /opt/puppet/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile RAILS_ENV=production \
-  nodegroup:add[cm4_embedded,profile::cloudera::agent4_server_embedded,skip] \
-  nodegroup:add[cm4_mysql,profile::cloudera::agent4_server_mysql,skip] \
-  nodegroup:add[cm4_postgresql,profile::cloudera::agent4_server_postgresql,skip] \
-  nodegroup:add[cm5_embedded,profile::cloudera::agent5_server_embedded,skip] \
-  nodegroup:add[cm5_mysql,profile::cloudera::agent5_server_mysql,skip] \
-  nodegroup:add[cm5_postgresql,profile::cloudera::agent5_server_postgresql,skip]
+#curl $CURL_OPTS -H "Content-Type:application/json" -d '{
+#    "name": "TEST",
+#    "parent": "00000000-0000-4000-8000-000000000000",
+#    "classes": {
+#        "epel": {}
+#    }
+#}' https://master:4433/classifier-api/v1/groups ; echo
 
-  /opt/puppet/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile RAILS_ENV=production \
-  nodegroup:add[cm5_noparcels_gplextras_cdh5,profile::cloudera::agent5_noparcels_gplextras_cdh5,skip] \
-  nodegroup:add[cm5_noparcels_gplextras_cdh4,profile::cloudera::agent5_noparcels_gplextras_cdh4,skip]
 
-  /opt/puppet/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile RAILS_ENV=production \
-  nodegroup:add[debian,locales,skip] \
-  nodegroup:addclass[debian,apt,skip] \
-  nodegroup:addclassparam[debian,apt,proxy_host,proxy] \
-  nodegroup:addclassparam[debian,apt,proxy_port,3128] \
-  node:add[debian6,debian,,skip] \
-  node:addgroup[debian6,debian] \
-  node:add[debian7,debian,,skip] \
-  node:addgroup[debian7,debian] \
-  node:add[ubuntu1004,debian,,skip] \
-  node:addgroup[ubuntu1004,debian] \
-  node:add[ubuntu1204,debian,,skip] \
-  node:addgroup[ubuntu1204,debian] \
-  node:add[ubuntu1404,debian,,skip] \
-  node:addgroup[ubuntu1404,debian]
+  #/opt/puppet/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile RAILS_ENV=production \
+  #nodegroup:add[cm4_embedded,profile::cloudera::agent4_server_embedded,skip] \
+  #nodegroup:add[cm4_mysql,profile::cloudera::agent4_server_mysql,skip] \
+  #nodegroup:add[cm4_postgresql,profile::cloudera::agent4_server_postgresql,skip] \
+  #nodegroup:add[cm5_embedded,profile::cloudera::agent5_server_embedded,skip] \
+  #nodegroup:add[cm5_mysql,profile::cloudera::agent5_server_mysql,skip] \
+  #nodegroup:add[cm5_postgresql,profile::cloudera::agent5_server_postgresql,skip]
+  curl $CURL_OPTS -H "Content-Type:application/json" -d '{
+    "name": "cm4_embedded",
+    "parent": "00000000-0000-4000-8000-000000000000",
+    "classes": {
+      "profile::cloudera::agent4_server_embedded": {}
+    }
+  }' https://master:4433/classifier-api/v1/groups ; echo
+  curl $CURL_OPTS -H "Content-Type:application/json" -d '{
+    "name": "cm4_mysql",
+    "parent": "00000000-0000-4000-8000-000000000000",
+    "classes": {
+      "profile::cloudera::agent4_server_mysql": {}
+    }
+  }' https://master:4433/classifier-api/v1/groups ; echo
+  curl $CURL_OPTS -H "Content-Type:application/json" -d '{
+    "name": "cm4_postgresql",
+    "parent": "00000000-0000-4000-8000-000000000000",
+    "classes": {
+      "profile::cloudera::agent4_server_postgresql": {}
+    }
+  }' https://master:4433/classifier-api/v1/groups ; echo
+  curl $CURL_OPTS -H "Content-Type:application/json" -d '{
+    "name": "cm5_embedded",
+    "parent": "00000000-0000-4000-8000-000000000000",
+    "classes": {
+      "profile::cloudera::agent5_server_embedded": {}
+    }
+  }' https://master:4433/classifier-api/v1/groups ; echo
+  curl $CURL_OPTS -H "Content-Type:application/json" -d '{
+    "name": "cm5_mysql",
+    "parent": "00000000-0000-4000-8000-000000000000",
+    "classes": {
+      "profile::cloudera::agent5_server_mysql": {}
+    }
+  }' https://master:4433/classifier-api/v1/groups ; echo
+  curl $CURL_OPTS -H "Content-Type:application/json" -d '{
+    "name": "cm5_postgresql",
+    "parent": "00000000-0000-4000-8000-000000000000",
+    "classes": {
+      "profile::cloudera::agent5_server_postgresql": {}
+    }
+  }' https://master:4433/classifier-api/v1/groups ; echo
 
-  /opt/puppet/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile RAILS_ENV=production \
-  nodegroup:add[redhat,selinux,skip] \
-  nodegroup:addclassparam[redhat,selinux,mode,disabled] \
-  node:add[centos5,redhat,,skip] \
-  node:addgroup[centos5,redhat] \
-  node:add[centos6,redhat,,skip] \
-  node:addgroup[centos6,redhat]
+  #/opt/puppet/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile RAILS_ENV=production \
+  #nodegroup:add[cm5_noparcels_gplextras_cdh5,profile::cloudera::agent5_noparcels_gplextras_cdh5,skip] \
+  #nodegroup:add[cm5_noparcels_gplextras_cdh4,profile::cloudera::agent5_noparcels_gplextras_cdh4,skip]
+  curl $CURL_OPTS -H "Content-Type:application/json" -d '{
+    "name": "cm5_noparcels_gplextras_cdh4",
+    "parent": "00000000-0000-4000-8000-000000000000",
+    "classes": {
+      "profile::cloudera::agent5_noparcels_gplextras_cdh4": {}
+    }
+  }' https://master:4433/classifier-api/v1/groups ; echo
+  curl $CURL_OPTS -H "Content-Type:application/json" -d '{
+    "name": "cm5_noparcels_gplextras_cdh5",
+    "parent": "00000000-0000-4000-8000-000000000000",
+    "classes": {
+      "profile::cloudera::agent5_noparcels_gplextras_cdh5": {}
+    }
+  }' https://master:4433/classifier-api/v1/groups ; echo
+
+  #/opt/puppet/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile RAILS_ENV=production \
+  #nodegroup:add[debian,locales,skip] \
+  #nodegroup:addclass[debian,apt,skip] \
+  #nodegroup:addclassparam[debian,apt,proxy_host,proxy] \
+  #nodegroup:addclassparam[debian,apt,proxy_port,3128] \
+  #node:add[debian6,debian,,skip] \
+  #node:addgroup[debian6,debian] \
+  #node:add[debian7,debian,,skip] \
+  #node:addgroup[debian7,debian] \
+  #node:add[ubuntu1004,debian,,skip] \
+  #node:addgroup[ubuntu1004,debian] \
+  #node:add[ubuntu1204,debian,,skip] \
+  #node:addgroup[ubuntu1204,debian] \
+  #node:add[ubuntu1404,debian,,skip] \
+  #node:addgroup[ubuntu1404,debian]
+  curl $CURL_OPTS -H "Content-Type:application/json" -d '{
+    "name": "debian",
+    "parent": "00000000-0000-4000-8000-000000000000",
+    "classes": {
+      "apt": {
+        "proxy_host": "proxy",
+        "proxy_port": "3128"
+      },
+      "locales": {}
+    },
+    "rule": [
+      "or",
+      [ "=", "name", "debian6" ],
+      [ "=", "name", "debian7" ],
+      [ "=", "name", "ubuntu1004" ],
+      [ "=", "name", "ubuntu1204" ],
+      [ "=", "name", "ubuntu1404" ]
+    ]
+  }' https://master:4433/classifier-api/v1/groups ; echo
+
+  #/opt/puppet/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile RAILS_ENV=production \
+  #nodegroup:add[redhat,selinux,skip] \
+  #nodegroup:addclassparam[redhat,selinux,mode,disabled] \
+  #node:add[centos5,redhat,,skip] \
+  #node:addgroup[centos5,redhat] \
+  #node:add[centos6,redhat,,skip] \
+  #node:addgroup[centos6,redhat]
+  curl $CURL_OPTS -H "Content-Type:application/json" -d '{
+    "name": "redhat",
+    "parent": "00000000-0000-4000-8000-000000000000",
+    "classes": {
+      "selinux": {
+        "mode": "disabled"
+      }
+    },
+    "rule": [
+      "or",
+      [ "=", "name", "centos5" ],
+      [ "=", "name", "centos6" ]
+    ]
+  }' https://master:4433/classifier-api/v1/groups ; echo
 fi
